f1f66426223551a70ab0f495f7810b3b65b07b96
Diego Galindo <diegogalindo84@gmail.com>
2022-10-21 19:47:50 -0300

    first commit

---
+++ src/ConexaoDAO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Diego
+ */
+
+
+public class ConexaoDAO {
+    public Connection conectaBD(){
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost:3606/bancoteste?user=root&password=";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return conn;
+    }
+}
