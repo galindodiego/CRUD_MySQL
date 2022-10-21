3fa7743ecde8a05dcb5b4b3cdcac8f66b3f04406
Diego Galindo <diegogalindo84@gmail.com>
2022-10-21 20:44:08 -0300

    Pacotes DAO, DTO, VIEW e tela de login

--- src/DAO/ConexaoDAO.java
+++ src/DAO/ConexaoDAO.java
@@ -1,3 +1,5 @@
+package DAO;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -6,8 +8,6 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-
 /**
  * 
  * @author Diego
